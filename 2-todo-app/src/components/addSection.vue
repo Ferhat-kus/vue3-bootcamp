0130789153ff81f2ed433408f3bdbe338539029c
Ferhat-kus <145162091+Ferhat-kus@users.noreply.github.com>
2024-05-22 20:11:29 +0300

    Vue cli todo app (#4)
    * feat: vue-cli
    
    * feat: style css added
    
    * feat: firt vue application
    
    * firt commit
    
    * feat: style css added
    
    * feat: provide, inject, emit, event, components data transfer between learned

---
+++ 2-todo-app/src/components/addSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+    type="text"
+    @keypress.enter="addNewTodo(todoText, todoText=null)"
+    placeholder="Birşeyler yazınızs"
+    v-model="todoText"
+  />
+</template>
+<script>
+export default {
+  props: {
+    addNewTodo: {
+      type: Function,
+    },
+  },
+  data() {
+    return {
+      todoText: null,
+    };
+  },
+  methods: {
+    // addNewTodo() {
+    //   this.$emit("add-todo", this.todoText);
+    //   this.todoText = null;
+    // },
+  },
+};
+</script>
---
+++ 2-todo-app/src/components/todoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <todoListItem
+      v-for="item in provideData.todoList"
+      :key="item.id"
+      :item="item"
+    />
+  </ul>
+</template>
+<script>
+import todoListItem from "@/components/todoListItem.vue";
+export default {
+  inject: ["provideData"],
+  components: {
+    todoListItem,
+  },
+};
+</script>
