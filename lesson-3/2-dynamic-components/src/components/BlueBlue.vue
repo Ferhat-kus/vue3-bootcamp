a779ed194e446ec71ca560c8a10ffa1eaf07e378
Ferhat-kus <145162091+Ferhat-kus@users.noreply.github.com>
2024-05-27 16:48:03 +0300

    feat: dynamic components

---
+++ lesson-3/2-dynamic-components/src/components/BlueBlue.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-blue">Blue</div>
+  <input type="text" />
+</template>
+<script>
+export default {
+  mounted() {
+    console.log("blue MOUNTED");
+  },
+  activated() {
+    console.log("Active Blue");
+  },
+  deactivated() {
+    console.log("deactive Blue");
+  },
+};
+</script>
