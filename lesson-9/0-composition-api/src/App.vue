15af3a18d64cd31250706984e9cd642d2770ef6a
Ferhat-kus <145162091+Ferhat-kus@users.noreply.github.com>
2024-06-04 02:26:35 +0300

    feat: ref, method and computed usage learned

--- lesson-9/0-composition-api/src/App.vue
+++ lesson-9/0-composition-api/src/App.vue
@@ -1,9 +1,41 @@
 <template>
-  <div>
+  <h3>{{ title }}</h3>
+  <input v-model="title" type="text" />
+  <h3>Toplam {{ titleLenghtMessage }}</h3>
 
-  </div>
+  <button @click="toggleIt">toggle</button>
+  <p v-if="show">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ea eius
+    corrupti nemo optio similique, voluptates maiores fugiat, adipisci harum
+    asperiores, ex molestiae eligendi numquam. Odit aspernatur nesciunt vel non!
+  </p>
 </template>
 
-<script setup>
-
+<script>
+import { computed, ref } from "vue";
+export default {
+  // data() {
+  //   return {
+  //     title: "Bu bir başlık Abi"
+  //   }
+  // },
+  setup() {
+    const title = ref("Merhaba");
+    const show = ref(false);
+    // const titleLenghtMessage = computed(() => {
+    //   return title.value.length + ' adet karkter var'
+    // })
+    const titleLenghtMessage = computed(() => title.value.length + ' adet karkter var')
+    const toggleIt = () => {
+      show.value = !show.value
+    }
+    
+    return {
+      title,
+      show,
+      toggleIt,
+      titleLenghtMessage,
+    };
+  },
+};
 </script>
