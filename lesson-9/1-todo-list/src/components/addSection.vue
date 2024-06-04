3dbf79a1a3290fbc53dacc5515d4628cce4c69fe
Ferhat-kus <145162091+Ferhat-kus@users.noreply.github.com>
2024-06-04 15:02:31 +0300

    Lesson 9.1 (#7)
    * feat: creted project
    
    * feat: vuex store manegment
    
    * feat: style.css added
    
    * feat: style.css added
    
    * feat: The working environment was prepared appropriately
    
    * feat: ref, method and computed usage learned
    
    * feat: watch , watchEffect and composebles usage learned
    
    * feat: create new project
    
    * feat: tailwind added
    
    * feat: tailwind configration 👍
    
    * feat: Learned how to use props and emis in setup

---
+++ lesson-9/1-todo-list/src/components/addSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section>
+      <input
+        placeholder="Nedir?"
+        @keydown.enter="newItem"
+        type="text"
+        class="w-full p-1 bg-gray-700 border border-gray-400 rounded-md outline-none"
+      />
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    AddTodo: Function,
+  },
+  setup(props) {
+    const newItem = (event) => {
+      props.AddTodo(event.target.value);
+      event.target.value = ''
+    };
+    return { newItem };
+  },
+};
+</script>
---
+++ lesson-9/1-todo-list/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  purge: ["./src/**/*.vue"],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
