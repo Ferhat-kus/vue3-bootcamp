5c445303efbd822be9386dd137c494319d4078be
Ferhat-kus <145162091+Ferhat-kus@users.noreply.github.com>
2024-06-05 08:47:48 +0300

    feat: defineProps, defineEmits, lifecycleHook and composible learned 👍

--- lesson-10/0-script-setup/src/App.vue
+++ lesson-10/0-script-setup/src/App.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    
+    <h3>{{ title }}</h3>
+    <input v-model="title" type="text" />
+    <button @click="inc">{{ counter }}</button>
+    <hr />
+    <oddOrEven @odd-event="alertMe" :counter="counter" />
+    <hr />
+    <h1>User</h1>
+    <input v-model="state.personal.name" type="text" />
+    <input v-model="state.personal.lname" type="text" />
+    {{ state.personal.name }} - {{ state.personal.lname }}
   </div>
 </template>
+
+<script setup>
+import oddOrEven from "./components/oddOrEven.vue";
+import Utils from "./composeble/Utils.js";
+const { title, counter, inc, alertMe } = Utils();
+import { reactive } from "vue";
+const state = reactive({
+  personal: {
+    name: "abuzer",
+    lname: "zühtü",
+  },
+});
+</script>
---
+++ lesson-10/0-script-setup/src/components/oddOrEven.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ result }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { computed, watch } from "vue";
+const props = defineProps({
+  counter: Number,
+});
+const emit = defineEmits(["odd-event"]);
+const result = computed(() => (props.counter % 2 == 0 ? "Cift" : "Tek"));
+watch(result, (result) => {
+  if (result == "Tek") emit("odd-event", true);
+});
+</script>
